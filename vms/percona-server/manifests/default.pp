9cde0a75b84d419d0aab43b32cdfe20dbc24c053
Fernando Ipar <ipar.fernando@gmail.com>
2013-09-09 12:30:19 -0300

    changes to percona-server base vm

--- vms/percona-server/manifests/default.pp
+++ vms/percona-server/manifests/default.pp
@@ -17,11 +17,11 @@ class percona {
 
 # include the packages needed for PXC
         package {
-                "Percona-Server-server-55.$hardwaremodel":
+                "Percona-Server-server-51.$hardwaremodel":
                         alias => "MySQL-server",
                         require => [ Yumrepo['percona'], Package['MySQL-client'] ],
                         ensure => "installed";
-                "Percona-Server-client-55.$hardwaremodel":
+                "Percona-Server-client-51.$hardwaremodel":
                         alias => "MySQL-client",
                         require => Yumrepo['percona'],
                         ensure => "installed";
