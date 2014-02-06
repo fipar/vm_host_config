ada8b56a5a391f0440ceee72c5d5020269f104e0
Fernando Ipar <ipar.fernando@gmail.com>
2014-02-06 12:58:39 -0200

    added postgresql 9.3 vm

---
+++ vms/postgresql/provision.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+#provisioner for centos 6 based box
+
+[ -f /etc/yum.repos.d/pgdg-93-centos.repo ] || {
+    rpm -ivh http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
+    yum -y update
+    yum -y install postgresql93 postgresql93-server 
+}
+
+
