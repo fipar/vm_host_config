9bd4157d87b1993c41e4190137a3513816c6a4ba
Fernando Ipar <ipar.fernando@gmail.com>
2014-02-11 13:32:54 -0200

    removed .vagrant dir, added pgsql contrib package to provisioning script

--- vms/postgresql/provision.sh
+++ vms/postgresql/provision.sh
@@ -4,7 +4,7 @@
 [ -f /etc/yum.repos.d/pgdg-93-centos.repo ] || {
     rpm -ivh http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
     yum -y update
-    yum -y install postgresql93 postgresql93-server 
+    yum -y install postgresql93 postgresql93-server postgresql93-contrib
 }
 
 
