971848f3ed472cdd4e20884f07d17361e21f4043
Fernando Ipar <ipar.fernando@gmail.com>
2014-02-16 17:26:07 -0200

    fixed provisioner for neo4j

--- vms/neo4j/provision.sh
+++ vms/neo4j/provision.sh
@@ -4,8 +4,6 @@
 dpkg -l|grep neo4j>/dev/null || {
 	wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add - 
 	echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
-	apt-get update
-	apt-get install neo4j 
-	echo 'neo4j start' >> /etc/rc.local
-	neo4j start 
+	apt-get -y update
+	apt-get -y install neo4j 
 }
